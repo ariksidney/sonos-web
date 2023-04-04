5d598e13d7e10b07505e1797d47da01acba8a9eb
ariksidney <hello@arik.sydney>
2023-04-04 22:48:22 +0200

    Adjustments to navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -8,6 +8,9 @@
             <button @click="overview()">Overview</button>
         </div>
         <div class="hover:text-violet-500 dark:hover:text-violet-400">
+            <button @click="favorites()">Favorites</button>
+        </div>
+        <div class="hover:text-violet-500 dark:hover:text-violet-400">
             <button @click="about()">About</button>
         </div>
         <div class="hover:text-violet-500 dark:hover:text-violet-400">
@@ -15,7 +18,7 @@
         </div>
     </aside>
     <aside class="relative h-full md:invisible pt-4 pr-4 pl-4 text-slate-900 dark:text-white">
-        <div class="flex gap-3">
+        <div class="flex gap-3 pb-5">
             <button @click="toggleMobileMenu()" class="outline-none mobile-menu-button">
                 <svg class=" w-6 h-6 text-gray-500 hover:text-violet-500" x-show="!showMenu" fill="none"
                     stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
@@ -30,6 +33,7 @@
         <div class="hidden mobile-menu mb-7 mt-3">
             <div class="flex flex-col gap-2 w-full">
                 <button class="hover:text-violet-500 dark:hover:text-violet-400" @click="overview()">Overview</button>
+                <button class="hover:text-violet-500 dark:hover:text-violet-400" @click="favorites()">Favorites</button>
                 <button class="hover:text-violet-500 dark:hover:text-violet-400" @click="about()">About</button>
                 <button class="hover:text-violet-500 dark:hover:text-violet-400" @click="logout()">Logout</button>
             </div>
@@ -64,6 +68,9 @@ export default {
         },
         about() {
             this.router.push('/about')
+        },
+        favorites() {
+            this.router.push('/favorites')
         },
         toggleMobileMenu() {
             const mobile = document.querySelector(".mobile-menu");
