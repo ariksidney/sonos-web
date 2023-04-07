00636a6a2ada27dfc40646697dc2c0543492c5ba
ariksidney <hello@arik.sydney>
2023-04-07 22:43:04 +0200

    Add basic pwa support and eslint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
 import { createRouter, createWebHistory } from "vue-router";
 import SignIn from "./components/SignIn.vue";
 import Overview from "./components/Overview.vue";
@@ -10,22 +11,27 @@ import Favorites from './components/Favorites.vue';
 import AuthService from './services/auth.service';
 
 const routes = [
-    { path: "/", component: Overview },
-    { path: "/SignIn", component: SignIn },
-    { path: "/Settings", component: Settings },
-    { path: "/about", component: About },
-    { path: "/favorites", component: Favorites },
-  ];
+  { path: "/", component: Overview },
+  { path: "/SignIn", component: SignIn },
+  { path: "/Settings", component: Settings },
+  { path: "/about", component: About },
+  { path: "/favorites", component: Favorites },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-  });
+  history: createWebHistory(),
+  routes
+});
 
-  router.beforeEach(async (to, from) => {
-    if (!AuthService.isLoggedIn() && to.path !== '/SignIn') {
-      return '/SignIn'
-    }
-  })
+router.isReady().then(async () => {
+  const { registerSW } = await import('virtual:pwa-register')
+    registerSW({ immediate: true })
+})
+
+router.beforeEach(async (to, from) => {
+  if (!AuthService.isLoggedIn() && to.path !== '/SignIn') {
+    return '/SignIn'
+  }
+})
 
 createApp(App).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: 'env',
-  plugins: [vue()]
+  plugins: [vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'inline'
+    })
+  ],
+  server: {
+    port: 3000
+  },
 })
